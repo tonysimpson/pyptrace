38c289994a35e95f2287749d70adc94f762277c0
wenlin.wu <wenlin.wu@outlook.com>
2017-08-15 20:44:07 +0800

    add several other intefaces

--- ptrace/pyptrace.c
+++ ptrace/pyptrace.c
@@ -1,5 +1,7 @@
-#include <Python.h>
 #include <sys/ptrace.h>
+#include <errno.h>
+
+#include <Python.h>
 
 static PyObject* traceme(PyObject* self)
 {
@@ -15,7 +17,7 @@ static PyObject* attach(PyObject *self, PyObject *args)
     long pid;
 
     if (!PyArg_ParseTuple(args, "l", &pid)) {
-	return NULL;
+        return NULL;
     }
 
     ret = ptrace(PTRACE_ATTACH, pid, NULL, NULL);
@@ -28,7 +30,7 @@ static PyObject* cont(PyObject *self, PyObject *args)
     long pid;
 
     if (!PyArg_ParseTuple(args, "l", &pid)) {
-	return NULL;
+        return NULL;
     }
 
     ret = ptrace(PTRACE_CONT, pid, NULL, NULL);
@@ -42,37 +44,123 @@ static PyObject* setoptions(PyObject *self, PyObject *args)
     long options;
 
     if (!PyArg_ParseTuple(args, "ll", &pid, &options)) {
-	return NULL;
+        return NULL;
     }
 
     ret = ptrace(PTRACE_SETOPTIONS, pid, NULL, (void *) options);
+
     return Py_BuildValue("l", ret);
+}
+
+static PyObject* peekdata(PyObject *self, PyObject *args)
+{
+    long ret;
+    long pid;
+    long addr;
+
+    if (!PyArg_ParseTuple(args, "ll", &pid, &addr)) {
+        return NULL;
+    }
+
+    errno = 0;
+    ret = ptrace(PTRACE_PEEKDATA, pid, (void *) addr, NULL);
+
+    return Py_BuildValue("(li)", ret, errno);
+}
+
+static PyObject* pokedata(PyObject *self, PyObject *args)
+{
+    long ret;
+    long pid;
+    long addr;
+    long data;
+
+    if (!PyArg_ParseTuple(args, "lll", &pid, &addr, &data)) {
+        return NULL;
+    }
+
+    errno = 0;
+    ret = ptrace(PTRACE_POKEDATA, pid, (void *) addr, data);
+
+    return Py_BuildValue("(li)", ret, errno);
+}
+
+static PyObject* peektext(PyObject *self, PyObject *args)
+{
+    long ret;
+    long pid;
+    long addr;
+
+    if (!PyArg_ParseTuple(args, "ll", &pid, &addr)) {
+        return NULL;
+    }
+
+    errno = 0;
+    ret = ptrace(PTRACE_PEEKTEXT, pid, (void *) addr, NULL);
+
+    return Py_BuildValue("(li)", ret, errno);
+}
+
+static PyObject* poketext(PyObject *self, PyObject *args)
+{
+    long ret;
+    long pid;
+    long addr;
+    long data;
+
+    if (!PyArg_ParseTuple(args, "lll", &pid, &addr, &data)) {
+        return NULL;
+    }
+
+    errno = 0;
+    ret = ptrace(PTRACE_POKETEXT, pid, (void *) addr, data);
+
+    return Py_BuildValue("(li)", ret, errno);
 }
 
 static PyMethodDef pyptrace_funcs[] = {
     {
-	"traceme",
-	(PyCFunction) traceme, 
-	METH_NOARGS,
+        "traceme",
+        (PyCFunction) traceme, 
+        METH_NOARGS,
     	"Indicate that this process is to be traced by its parent.\n"
     }, {
-	"attach",
-	(PyCFunction) attach,
-	METH_VARARGS,
-	"Attach  to  the process specified in pid, making it a tracee of the calling process.\n"
+        "attach",
+        (PyCFunction) attach,
+        METH_VARARGS,
+        "Attach  to  the process specified in pid, making it a tracee of the calling process.\n"
     }, {
-	"cont",
-	(PyCFunction) cont,
-	METH_VARARGS,
-	"Restart  the  stopped  tracee  process.\n"
+        "cont",
+        (PyCFunction) cont,
+        METH_VARARGS,
+        "Restart  the  stopped  tracee  process.\n"
     }, {
-	"setoptions",
-	(PyCFunction) setoptions,
-	METH_VARARGS,
-	"Set  ptrace  options from data.\n"
-
+        "setoptions",
+        (PyCFunction) setoptions,
+        METH_VARARGS,
+        "Set  ptrace  options from data.\n"
     }, {
-	NULL
+        "peekdata",
+        (PyCFunction) peekdata,
+        METH_VARARGS,
+        "",
+    }, {
+        "pokedata",
+        (PyCFunction) pokedata,
+        METH_VARARGS,
+        "TODO",
+    }, {
+        "peektext",
+        (PyCFunction) peektext,
+        METH_VARARGS,
+        "TODO"
+    }, {
+        "poketext",
+        (PyCFunction) poketext,
+        METH_VARARGS,
+        "TODO"
+    }, {
+        NULL
     }
 };
 
---
+++ ptrace/test.py
@@ -0,0 +1,4 @@
+import pyptrace
+
+print pyptrace.peekdata(1, 0x008)
+print pyptrace.pokedata(1, 0x008, 0x02)
